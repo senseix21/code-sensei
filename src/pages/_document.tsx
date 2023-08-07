71aaa56c73252129d3a1b2386377404cb5f6a8ee
senseiX21 <mahdix2118@protonmail.com>
2023-08-07 07:25:10 +0100

    Added gsc verification

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="yQNQ64jnRCeR_ShJr_RkAW6gUneuVKGODG-TO340y9I" />
+      </Head>
       <body>
         <Main />
         <NextScript />
