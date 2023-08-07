f160756cfcddcc903eefe6b31caa704b08eb61bf
senseiX21 <mahdix2118@protonmail.com>
2023-08-07 07:53:18 +0100

    added bing console config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="google-site-verification" content="yQNQ64jnRCeR_ShJr_RkAW6gUneuVKGODG-TO340y9I" />
+        <meta name="msvalidate.01" content="0E61D15BDA029F14E3734ACC830A5266" />
       </Head>
       <body>
         <Main />
